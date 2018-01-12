1b8153d827ad6931aecd1ede690999fa492a5e75
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-12 13:01:32 -0800

    Got part2 fixed and code reviewed and working on part3

--- melons.py
+++ melons.py
@@ -4,7 +4,7 @@
 class AbstractMelonOrder(object):
     """An abstract base class that other Melon Orders inherit from"""
 
-    def __init__(self, species, qty, country_code=None):
+    def __init__(self, species, qty, country_code=None, passed_inspection=False):
         """Initialize melon order attributes."""
         # creates the instance
         self.species = species
@@ -19,8 +19,11 @@ class AbstractMelonOrder(object):
     def get_total(self):
         """Calculate price, including tax."""
         # method on the class DomesticMelonOrder
-
         base_price = 5
+
+        if self.species == "Christmas melons":
+            base_price = base_price * 1.5
+
         total = (1 + self.tax) * self.qty * base_price
 
         return total
@@ -48,6 +51,8 @@ class InternationalMelonOrder(AbstractMelonOrder):
         self.country_code = country_code
         self.tax = 0.17
         self.order_type = "international"
+        self.species = species # added species for part 2
+        self.qty = qty # added qty for part 2
         # this has to be self because it's in the __init__
         # ...and therefore is on the object, not the class
 
@@ -58,3 +63,35 @@ class InternationalMelonOrder(AbstractMelonOrder):
         return self.country_code
         # international has country code; domestic does not
         # WAI???
+
+    def get_total(self):
+        """Fee calculation for int'l order"""
+
+        subtotal = super(InternationalMelonOrder, self).get_total()
+        if self.qty < 10:
+            total = subtotal + 3
+
+        return total
+
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    """No tax for all gov't orders"""
+
+    # what is specific to this class?
+    def __init__(self, species, qty, country_code, passed_inspection):
+
+        self.passed_inspection = False
+        self.tax = 0
+
+    def mark_inspection(self):
+        """Record the fact than an order has been shipped."""
+
+        self.passed_inspection = True
+
+    def get_total(self):
+        """Not tax for gov't orders"""
+        subtotal = super(GovernmentMelonOrder, self).get_total()
+
+        total = self.qty * base_price
+
+        return total

cc5fa965aa3480cbebfcfb6497fafdbd67833073
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-12 12:25:21 -0800

    Part 1 - refactored & code reviewed. :)

--- melons.py
+++ melons.py
@@ -1,9 +1,10 @@
 """Classes for melon orders."""
 
+
 class AbstractMelonOrder(object):
     """An abstract base class that other Melon Orders inherit from"""
 
-    def __init__(self, species, qty, country_code = None):
+    def __init__(self, species, qty, country_code=None):
         """Initialize melon order attributes."""
         # creates the instance
         self.species = species
@@ -14,6 +15,7 @@ class AbstractMelonOrder(object):
         # if country code isn't none or false, will set country code attribute
         # ...as entered country code
 
+
     def get_total(self):
         """Calculate price, including tax."""
         # method on the class DomesticMelonOrder
@@ -22,11 +24,10 @@ class AbstractMelonOrder(object):
         total = (1 + self.tax) * self.qty * base_price
 
         return total
-        # moved from DomesticMelonOrder
+
 
     def mark_shipped(self):
         """Record the fact than an order has been shipped."""
-        # method on the class DomesticMelonOrder
 
         self.shipped = True
         # moved from DomesticMelonOrder
@@ -42,21 +43,18 @@ class DomesticMelonOrder(AbstractMelonOrder):
 class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
 
-    # def __init__(self, species, qty, country_code):
-    #     """Initialize melon order attributes."""
-    #     # creates the instance
+    def __init__(self, species, qty, country_code):
 
-    #     self.species = species
-    #     self.qty = qty
-    #     self.country_code = country_code
-    #     self.shipped = False
-    #     self.order_type = "international"
-    #     self.tax = 0.17
-    tax = 0.17
-    order_type = "international"
+        self.country_code = country_code
+        self.tax = 0.17
+        self.order_type = "international"
+        # this has to be self because it's in the __init__
+        # ...and therefore is on the object, not the class
 
     def get_country_code(self):
         """Return the country code."""
         #method on the class InternationalMelonOrder
 
         return self.country_code
+        # international has country code; domestic does not
+        # WAI???
